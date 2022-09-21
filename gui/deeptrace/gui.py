f39153431e5afae27db848bce634ac400b445348
Joao Couto <jpcouto@gmail.com>
2022-09-21 11:08:19 -0700

    Started gui widgets.

--- gui/deeptrace/gui.py
+++ gui/deeptrace/gui.py
@@ -30,6 +30,7 @@ from PyQt5.QtWidgets import (QWidget,
 from PyQt5.QtGui import QImage, QPixmap,QBrush,QPen,QColor,QFont
 from PyQt5.QtCore import Qt,QSize,QRectF,QLineF,QPointF,QTimer
 import sys
+from .utils import *
 
 class DeepTraCE(QMainWindow):
     app = None
@@ -38,7 +39,28 @@ class DeepTraCE(QMainWindow):
         Graphical interface for the DeepTraCE pipeline.
         '''
         super(DeepTraCE,self).__init__()
+        mainw = QWidget()
+        self.setCentralWidget(mainw)
+        lay = QHBoxLayout()
+        mainw.setLayout(lay)
+        self.analysis_steps = dict(trailmap=True)
+        w = QGroupBox('TrailMap')
+        l = QFormLayout()
+        w.setLayout(l)
+        run_trailmap = QCheckBox()
+        run_trailmap.setChecked(self.analysis_steps['trailmap'])
+        def tmap_update(val):
+            self.analysis_steps['trailmap'] = not self.analysis_steps['trailmap']
+        run_trailmap.stateChanged.connect(tmap_update)
+        label = QLabel("Perform analysis: ")
+        info = '''Run cell or axon discovery using a TrailMap model.'''
+        label.setToolTip(info)
+        run_trailmap.setToolTip(info)
+        l.addRow(label,run_trailmap)
+        lay.addWidget(w)
+        
         self.show()
+
 def main():
     from argparse import ArgumentParser, RawDescriptionHelpFormatter
     import os
