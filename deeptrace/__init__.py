a9b590d20c6ffb9953c2285760f1e1b2acbb73cb
Joao Couto <jpcouto@gmail.com>
2023-03-28 12:47:55 -0700

    Include combine models in import.

--- deeptrace/__init__.py
+++ deeptrace/__init__.py
@@ -9,5 +9,6 @@ from .analysis import (BrainStack, read_atlas, load_deeptrace_models,
                        trailmap_segment_tif_files,
                        trailmap_list_models,
                        refine_connected_components,
-                       skeletonize_multithreshold_uint8_stack)
+                       skeletonize_multithreshold_uint8_stack,
+                       combine_models)
 
